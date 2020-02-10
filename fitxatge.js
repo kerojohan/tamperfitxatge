5e70b95bc31df6d6468c910bae71dc04c0512b6b
Johan <keroseno@gmail.com>
2020-02-10 14:29:07 +0100

    taula hores setmanals

--- fitxatge.js
+++ fitxatge.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fitxatge
 // @namespace    http://tampermonkey.net/
-// @version      0.461
+// @version      0.463
 // @description  Plugin no oficial eina fitxatge
 // @author       You
 // @match        https://fitxatge.csuc.cat/marcajes.php
@@ -9,6 +9,22 @@
 // @updateURL   https://raw.githubusercontent.com/kerojohan/tamperfitxatge/master/fitxatge.js
 // @downloadURL https://raw.githubusercontent.com/kerojohan/tamperfitxatge/master/fitxatge.js
 // ==/UserScript==
+
+function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d=new Date(d);
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return weekNo;
+}
+
 function isTodayEntry(dateText) {
     var inputDate = dateText.split(" ");
     inputDate = inputDate[0].split("-");
@@ -51,18 +67,18 @@ function calcul(entrades) {
     var lastValue;
     $.each(entrades, function(index, value) {
         // console.log( index + ": " + value);
-        // console.log(value );
-        if (inicibloc == "" && value[1].indexOf("Entrada") >= 0) {
-            inicibloc = (new Date(value[0])).getTime();
+        // console.log(value.hora_marcatge );
+        if (inicibloc == "" && value.tipus.indexOf("Entrada") >= 0) {
+            inicibloc = (new Date(value.hora_marcatge)).getTime();
             tancat = false;
         }
-        if (inicibloc != "" && fibloc == "" && value[1].indexOf("Entrada") == -1) {
-            tempstreballat = tempstreballat + ((new Date(value[0])).getTime() - inicibloc);
+        if (inicibloc != "" && fibloc == "" && value.tipus.indexOf("Entrada") == -1) {
+            tempstreballat = tempstreballat + ((new Date(value.hora_marcatge)).getTime() - inicibloc);
             inicibloc = "";
             fibloc = "";
             tancat = true;
         }
-        lastValue=value[0];
+        lastValue=value.hora_marcatge;
     });
     if (!tancat && inicibloc != "" && isTodayEntry(lastValue)) {
         var ara = new Date();
@@ -73,12 +89,13 @@ function calcul(entrades) {
 }
 
 
-function print(horesdia, horesmensuals) {
+function print(horesdia, horesmensuals,horessetmana) {
     // console.log(horesdia,horesmensuals);
     var today = new Date();
-    $("#hh").html(msToTime(horesdia,1));
+    $("#horesdiaactual").html(msToTime(horesdia,1));
 
-    $("#mh").html(msToTime(horesmensuals,2));
+    $("#horesmes").html(msToTime(horesmensuals,2)+"h");
+    $("#actualweekofYear").html(msToTime(horessetmana,2)+"h");
 
     var horestreballades = horesdia / (1000 * 60 * 60)
     if (horestreballades >= 8) {
@@ -90,69 +107,95 @@ function print(horesdia, horesmensuals) {
 
 (function() {
     'use strict';
-
-    var today = new Date();
+    var myObj = {}
+    myObj.today = new Date();
     var scope = $('#filtres > form:nth-child(1) > select').val();
     if (scope != "todos") {
 
         /*miro si veuen a tothom*/
-        var cella_entrades = 1;
+        var cella_tipus_entrades = 1;
         var cella2 = $("#infoMarcatges > table > thead > tr > th:nth-child(2)").html();
         if (cella2 == "NOM") {
-            cella_entrades = 2;
+            cella_tipus_entrades = 2;
         }
-        var entrades = [];
-        var entrades_mensuals = []; //on van tots els dies menys l'actual
+
+        myObj.entrades_avui = [];
+        myObj.entrades_mensuals = []; //on van tots els dies menys l'actual
         $("#infoMarcatges > table  tbody  tr").each(function() {
             var temps = $(this).find("td").first().html();
+            var weekinyear = getWeekNumber(temps);
             // console.log(temps);
-            if (cella_entrades == 2) {
-                if (isTodayEntry(temps)) {
-                    entrades.push([temps, $(this).find("td").first().next().next().html()]);
-                } else {
-                    entrades_mensuals.push([temps, $(this).find("td").first().next().next().html()]);
-                }
+            if (isTodayEntry(temps)) {
+                myObj.entrades_avui.push({"hora_marcatge":temps, "tipus":$(this).find("td:eq( "+cella_tipus_entrades+" )").html(),"setmanaAny":weekinyear});
             } else {
-                if (isTodayEntry(temps)) {
-                    entrades.push([temps, $(this).find("td").first().next().html()]);
-                } else {
-                    entrades_mensuals.push([temps, $(this).find("td").first().next().html()]);
-                }
+                myObj.entrades_mensuals.push({"hora_marcatge":temps, "tipus":$(this).find("td:eq( "+cella_tipus_entrades+" )").html(),"setmanaAny":weekinyear});
             }
         });
 
-
+        myObj.actualweekofYear=getWeekNumber(myObj.today);;
         /*si hi han entrades del dia actual posar rellotge*/
-        if (entrades.length > 0 || entrades_mensuals.length >0) {
-            $("#info").prepend("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\"><div class=\"plusinfo column\"></div><i class=\"fa fa-info-circle\" style=\"font-size:36px\"></i><div class=\"column\" id='nips' style=\"overflow-x:hidden; display:none\"></div>");
-            if (entrades.length > 0){
-                $(".plusinfo").append("<div id='hores'><h1  id=\"hh\" style=\"font-size:46px\"></h1></div>");
-                entrades = entrades.reverse();
+        if (myObj.entrades_avui.length > 0 || myObj.entrades_mensuals.length >0) {
+            $("#info").prepend("<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\
+<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/gh/kerojohan/tamperfitxatge@master/fitxatge2.css\">\
+<div class=\"plusinfo column\"></div><i class=\"fa fa-info-circle\" style=\"font-size:36px\" title=\"Clica per més informació\"></i><div class=\"column\" id='nips' style=\"overflow-x:hidden; display:none\"></div><div id=\"taulasetmanal\"/>");
+            if (myObj.entrades_avui.length > 0){
+                $(".plusinfo").append("<div><h1  id=\"horesdiaactual\" style=\"font-size:46px\"></h1></div>");
+                myObj.entrades_avui = myObj.entrades_avui.reverse();
             }
-            if(entrades_mensuals.length >0){
-		var user=$('#filtres > form > select[name="usuarioSeleccionado"] option:selected').text();  
-		if(user!=""){ user= user+" - "}
+            if(myObj.entrades_mensuals.length >0){
+                var user=$('#filtres > form > select[name="usuarioSeleccionado"] option:selected').text();
+                if(user!=""){ user= user+" - "}
                 var strmes=$('#filtres > form > select[name="mesSeleccionat"] option:selected').text();
-                $(".plusinfo").append("<div id='horesmensuals'><h2  style=\"font-size:24px;display: flex;\"><span>("+user+strmes+":&nbsp;</span><span id=\"mh\"></span><span>h)</span></h2></div>");
-                entrades_mensuals = entrades_mensuals.reverse();
+                $(".plusinfo").append("<div id='horesmensuals'><h2  style=\"font-size:24px;display: flex;\"><span>("+user+strmes+":&nbsp;</span><span id=\"horesmes\"></span><span>)</span></h2></div>");
+                myObj.entrades_mensuals = myObj.entrades_mensuals.reverse();
             }
-            var hmensual = calcul(entrades_mensuals);
+            var taulasetmanal="";
+            var totes_les_entrades=$.merge(myObj.entrades_mensuals, myObj.entrades_avui);
+            //console.log($.unique(totes_les_entrades.map(function (d) {return d.setmanaAny;})));
+
+
+            $.each($.unique(totes_les_entrades.map(function (d) {return d.setmanaAny;})), function(i, value)
+                   {
+                var entrades_setmanes;
+                var suma=0;
+                var idactual="";
+                //la setmana actual es tracte com suma de totes les altres entrades (estàtic) + suma d'entrades del dia (dinàmic)
+                if(myObj.actualweekofYear==value){
+                    entrades_setmanes=totes_les_entrades.filter(function (el) {return (el.setmanaAny==value && (new Date(el.hora_marcatge)).getDay()!=myObj.today.getDay());});
+                    myObj.setmanaencurs=entrades_setmanes;
+                    idactual="id=\"actualweekofYear\"";
+                    //console.log(entrades_setmanes)
+                    suma=calcul(entrades_setmanes)+calcul(myObj.entrades_avui);
+                }
+                else{
+                    entrades_setmanes=totes_les_entrades.filter(function (el) {return el.setmanaAny==value;});
+                    //console.log(entrades_setmanes)
+                    suma=calcul(entrades_setmanes);
+                }
+                taulasetmanal=taulasetmanal+"<tr><td>"+value+"</td><td "+idactual+">"+msToTime(suma)+"h</td></tr>"
+            });
+
+            $("#taulasetmanal").prepend("<table><thead><tr><th>Setmana</th><th>Hores</th></tr></thead><tbody>"+taulasetmanal+"</tbody></table>");
+
+            var hmensual = calcul(myObj.entrades_mensuals);
+            var horessetmana = calcul(myObj.setmanaencurs);
+            //console.log(myObj);
             setInterval(function() {
-                var hores_dia_actual = calcul(entrades);
-                print(hores_dia_actual, hmensual + hores_dia_actual);
+                var hores_dia_actual = calcul(myObj.entrades_avui);
+                print(hores_dia_actual, hmensual + hores_dia_actual,horessetmana+ hores_dia_actual);
             }, 1000);
         }
         // info
         $('#nips').append(" <script>  $(\".fa-info-circle\").click(function(){  $(\"#nips\").toggle();});</script> \
-                            <div style=\"width:100%;display: block;\" > \
-	<p><b>Del gener a mitjans de juny</b>, partida de dilluns a dijous i contínua el divendres per a un total  de  38  h/s.</p>  <ul style=\"width:100%;display: block;\">   \
-	<li>De dilluns a dijous: 8 hores diaries</li> <li>Divendres: 6 hores</li>  \
-	<li>Aturada mínima per dinar de 30 minuts</li>  \
-	</ul></div><div style=\"width:100%;display: block;\"><p><b>De  mitjans  de  juny  a  mitjans  de  setembre</b>  i  <b>de  Nadal  a  Reis</b></p>  \
-	<ul style=\"width:100%;display: block;\"><li>Jornada contínua  de  dilluns  a divendres, de 8.00 a 15.00 hores (35 h/s).</li>  \
-	<li>Aturada per esmorzar de 15 minuts</li>  </ul>     \
-	<p><b>De mitjans de setembre al desembre</b>, partida de dilluns a dijous i contínua el divendres per a un total de 37,5 h/s.</p><ul><li>De dilluns a dijous: 8 hores diaries</li> <li>Divendres: 5:30 hores</li>  \
-	<li>Aturada mínima per dinar de 30 minuts</li>  </ul> <p>*Extensió completament extraoficial, les dades mostrades poden no ser les correctes, el desenvolupador no es fa responsable del seu ús.</p>  <div> ");
+<div style=\"width:100%;display: block;\" > \
+<p><b>Del gener a mitjans de juny</b>, partida de dilluns a dijous i contínua el divendres per a un total  de  38  h/s.</p>  <ul style=\"width:100%;display: block;\">   \
+<li>De dilluns a dijous: 8 hores diaries</li> <li>Divendres: 6 hores</li>  \
+<li>Aturada mínima per dinar de 30 minuts</li>  \
+</ul></div><div style=\"width:100%;display: block;\"><p><b>De  mitjans  de  juny  a  mitjans  de  setembre</b>  i  <b>de  Nadal  a  Reis</b></p>  \
+<ul style=\"width:100%;display: block;\"><li>Jornada contínua  de  dilluns  a divendres, de 8.00 a 15.00 hores (35 h/s).</li>  \
+<li>Aturada per esmorzar de 15 minuts</li>  </ul>     \
+<p><b>De mitjans de setembre al desembre</b>, partida de dilluns a dijous i contínua el divendres per a un total de 37,5 h/s.</p><ul><li>De dilluns a dijous: 8 hores diaries</li> <li>Divendres: 5:30 hores</li>  \
+<li>Aturada mínima per dinar de 30 minuts</li>  </ul> <p>*Extensió completament extraoficial, les dades mostrades poden no ser les correctes, el desenvolupador no es fa responsable del seu ús.</p>  <div> ");
 
     }
 
